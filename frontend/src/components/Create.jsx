2ef794727365379da7186030d12a9a37d9a81698
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-12-15 22:03:52 -0500

    Add files via upload

---
+++ frontend/src/components/Create.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import './style.css'; 
+
+const Create = () => {
+  // state for form inputs
+  const [projectName, setProjectName] = useState('');
+  const [projectType, setProjectType] = useState('');
+  const [fundraisingGoal, setFundraisingGoal] = useState('');
+
+  // function for form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ projectName, projectType, fundraisingGoal });
+  };
+
+  return (
+    <div className="container">
+      <h2>Create a New Project</h2>
+      <form onSubmit={handleSubmit}>
+        <label className="form-label">
+          Project Name:
+          <input
+            className="form-input"
+            type="text"
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
+            required
+          />
+        </label>
+
+        <label className="form-label">
+          Project Type:
+          <select
+            className="form-input"
+            value={projectType}
+            onChange={(e) => setProjectType(e.target.value)}
+            required
+          >
+            <option value="">Select Project Type</option>
+            <option value="charity">Charity</option>
+            <option value="medical">Medical</option>
+            <option value="personl">Personal</option>
+          </select>
+        </label>
+
+        <label className="form-label">
+          Fundraising Goal:
+          <input
+            className="form-input"
+            type="number"
+            value={fundraisingGoal}
+            onChange={(e) => setFundraisingGoal(e.target.value)}
+            required
+          />
+        </label>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Create;
