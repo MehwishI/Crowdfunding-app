f2ca3e7e3b5bce08abf10933547babac992a1a42
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-12-15 22:37:56 -0500

    Update User.jsx

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -11,17 +11,17 @@ const User = () => {
           <p>content goes here</p>
         </div>
         <div className="section" id="funded">
-          {/* Content for the first section */}
+          {/* Content for the second section */}
           <h2>What you're funding</h2>
           <p>content goes here</p>
         </div>
         <div className="section" id="active">
-          {/* Content for the second section */}
+          {/* Content for the third section */}
           <h2>Active projects</h2>
           <p>content goes here</p>
         </div>
         <div className="section" id="create">
-          {/* Content for the third section */}
+          {/* Content for the fourth section */}
           <h2>Create your own project</h2>
           <p><a href="Create.jsx">
     <button>Create Project!</button>
